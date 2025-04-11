31ef721861c551e7b2a3899f9d014d2cdcd9a5ae
Paloma Tamayo <p.tamayo.segura@gmail.com>
2025-04-11 19:01:41 -0400

    modelo-certificado

---
+++ REST_VECINDAPP/Modelos/Certificados.cs
@@ -0,0 +1,13 @@
+﻿namespace REST_VECINDAPP.Modelos
+{
+    public class Certificado
+    {
+        public int id { get; set; }
+        public int solicitud_id { get; set; }
+        public string codigo_verificacion { get; set; }
+        public DateTime fecha_emision { get; set; }
+        public DateTime fecha_vencimiento { get; set; }
+        public string archivo_pdf { get; set; }
+        public string estado { get; set; }
+    }
+}
